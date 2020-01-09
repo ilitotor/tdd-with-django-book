793fc8285c411f83660593ae6843fa72ac37c4ae
Ilito Torquato <ilitotor@gmail.com>
2020-01-09 11:00:01 -0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,22 +17,37 @@ class NewVisitorTest(unittest.TestCase):
 
         # verify if title == 'To-do'
         self.assertIn('To-do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-do', header_text)
+
+        # try to add some task
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # add 'buy peacock feathers'
+        inputbox.send_keys('Buy peacock feathers')
+
+        # press enter, return list of tasks with '1 - buy peacock feathers'
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1 - buy peacock feathers' for row in rows)
+        )
+
+        # add one more item 'use peacock feathers to make a fly'
         self.fail('Finish the test')
 
-# try to add some task
+        # return list with 2 task
 
+        # url is unic and have a description text.
 
-# add 'buy peacock feathers'
-
-# press enter, return list of tasks with '1 - buy peacock feathers'
-
-# add one more item 'use peacock feathers to make a fly'
-
-# return list with 2 task
-
-# url is unic and have a description text.
-
-# access item by url
+        # access item by url
 
 # The end
 if __name__ == '__main__':
