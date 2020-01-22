09182b62b1de1f2b9effbc25c234fd134531072e
Ilito Torquato <ilitotor@gmail.com>
2020-01-22 10:51:14 -0800

    first steps of FT for layout+styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,9 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
+
 # import unittest
 
 MAX_WAIT = 10
+
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -29,44 +31,43 @@ class NewVisitorTest(LiveServerTestCase):
                 time.sleep(0.5)
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
-        #start a new list
+        # start a new list
         self.browser.get(self.live_server_url)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        #get a unique url
+        # get a unique url
         user1_list_url = self.browser.current_url
         self.assertRegex(user1_list_url, '/lists/.+')
 
-        #new user add a list
-        #new browser session
+        # new user add a list
+        # new browser session
         self.browser.quit()
         self.browser = webdriver.Chrome()
 
-        #verify if browser is empty
+        # verify if browser is empty
         self.browser.get(self.live_server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
-        #new user add new list/items
+        # new user add new list/items
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
-        #new user get a unique URL
+        # new user get a unique URL
         user2_list_url = self.browser.current_url
         self.assertRegex(user2_list_url, '/lists/.+')
         self.assertNotEqual(user2_list_url, user1_list_url)
 
-        #check if user1 items are in user2 list
+        # check if user1 items are in user2 list
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
-
 
     def test_can_star_a_list_for_one_user(self):
         # user access the home page
@@ -98,7 +99,23 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
 
-        # url is unique and have a description text.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
 
-        # access item by url
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
